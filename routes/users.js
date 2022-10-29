d4e36d0f525f8667fa11d462bf593630904ec78f
souravrane <sourav1121997@gmail.com>
2022-10-29 21:55:59 +0530

    setup liniting and husky

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", (req, res) => {
+  res.send("respond with a resource");
+});
+
+module.exports = router;
